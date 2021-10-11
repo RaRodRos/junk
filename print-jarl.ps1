21850954d0e67a84c0488341829a05be1eefebde
Ra! <62754237+RaRodRos@users.noreply.github.com>
2021-10-11 13:52:19 +0200

    PSCommandPath test

--- print-jarl.ps1
+++ print-jarl.ps1
@@ -1,6 +1,6 @@
 do {
-	echo "jarl"
-	$PSCommandPath
-	$MyInvocation.MyCommand.Path
+	If (!(echo $PSCommandPath)) {
+		echo New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1')
+	}
 } while ($true)
 echo "unreachable"
