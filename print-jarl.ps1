6a5fd5ab6ce35af64041afb39243aba47e9b3240
Ra! <62754237+RaRodRos@users.noreply.github.com>
2021-10-11 14:28:52 +0200

    Update print-jarl.ps1

--- print-jarl.ps1
+++ print-jarl.ps1
@@ -1,8 +1,5 @@
-do {
-	If ($PSCommandPath) { echo $PSCommandPath }
-	Else {
-		$jarl = (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1")
-		echo $jarl
-	}
-} while ($true)
-echo "unreachable"
+If ($PSCommandPath) { echo $PSCommandPath }
+Else {
+	$jarl = (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1")
+	echo $jarl
+}
