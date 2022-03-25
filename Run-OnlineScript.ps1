36f44e00489dbe9377f495f9968a0f3d0ed3584e
Ra <rarodriguezrosado@gmail.com>
2022-03-25 14:37:41 +0100

    Move Run-OnlineScript to powerShell-scripts repo

--- Run-OnlineScript.ps1
+++
@@ -1,5 +0,0 @@
-if (!($script = $args[0])) {
-	$script = 'https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1'
-}
-$script = [Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString($script))
-Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command $script" -Verb RunAs
